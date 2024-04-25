d8e777b5ce46c1ffe92c9ebb07b4544de198ea0d
Protaso BV <hello@protaso.nl>
2024-04-25 14:17:42 +0300

    Updating stable version tag

--- playbook.sh
+++ playbook.sh
@@ -46,7 +46,7 @@
 
 ########## Global Variables ##########
 
-TAG_STABLE="protasobv/penaviation-scs:0.7.2"
+TAG_STABLE="protasobv/penaviation-scs:0.7.1"
 TAG_LATEST="protasobv/penaviation-scs:latest"
 CONF_ISBD="https://raw.githubusercontent.com/protasobv/penaviation-scs/main/isbd.conf"
 CONF_NAVROUTER="https://raw.githubusercontent.com/protasobv/penaviation-scs/main/navrouter.conf"
